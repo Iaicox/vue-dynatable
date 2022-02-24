cc758c8dbec822ca3e8760c8b6774e16e6825650
Iacox <ilyshk999@gmail.com>
2022-02-24 10:27:29 +0300

    - added icons pack

---
+++ src/assets/icons/index.js
@@ -0,0 +1,15 @@
+const req = require.context('./', true, /.svg$/)
+const keys = req.keys()
+const modules = keys
+	.map(req)
+	.reduce((obj, path, i) => {
+		const key = keys[i].replace(/^\.\/(.*)\.svg$/gm, '$1').replace(/[\/\\]/gm, '--')
+		if (key)
+			obj = {
+				...obj,
+				[key]: path,
+			}
+
+		return obj
+	}, {})
+module.exports = modules

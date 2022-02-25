8b6409da3a2382aa1b4361644cd2d2c60008721c
Iacox <ilyshk999@gmail.com>
2022-02-25 18:23:34 +0300

    - added webpack copy plugin to copy js file that collect all icons paths

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,37 @@
+const CopyPlugin = require("copy-webpack-plugin")
+
 module.exports = {
   filenameHashing: false,
+  configureWebpack: {
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          {
+            from: 'src/assets/icons/build.js',
+            to: 'img/index.js',
+            toType: 'file',
+          }
+        ],
+      })
+    ]
+  },
+
 
   chainWebpack: config => {
-    config.module
-      .rule('images')
-      .set('parser', {
-        dataUrlCondition: {
-          maxSize: 4 * 1024 // 4KiB
-        }
-      })
+    // config.module
+    //   .rule('images')
+    //   .set('parser', {
+    //     dataUrlCondition: {
+    //       maxSize: 4 * 1024 // 4KiB
+    //     }
+    //   })
   },
 
   publicPath: process.env.NODE_ENV === 'production'
-      ? '/'
-      : '/vue-dynatable/',
+    ? '/vue-dynatable/'
+    : '/',
   outputDir: process.env.NODE_ENV === 'production'
-      ? __dirname+'/lib'
-      : __dirname+'/demo',
+    ? __dirname+'/lib'
+    : __dirname+'/demo',
   productionSourceMap: false
 }
